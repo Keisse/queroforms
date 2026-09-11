612cd469d7c8a85f660c40a70062297a643ff106
Keisse Brandão <keissebrandao@gmail.com>
2026-09-11 10:00:13 -0300

    Cleanup: remove hotfixes CSS obsoletos da inicializacao

--- src/main.tsx
+++ src/main.tsx
@@ -17,9 +17,7 @@ import './styles/finalPolish.css';
 import './styles/contextCompact.css';
 import './styles/title28.css';
 import './styles/photoChoiceButtons.css';
-import './styles/certificateHotfix.css';
 import './styles/answerEmojiFallback.css';
-import './styles/builderHeaderCleanup.css';
 import './styles/specialInsightVisuals.css';
 import './styles/quizResponsiveHardening.css';
 
